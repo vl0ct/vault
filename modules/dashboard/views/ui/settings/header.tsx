48828b57ccd622f14596bd2346e50e8f357d6160
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-04-30 17:00:10 +0530

    feat: del and leave

--- modules/dashboard/views/ui/settings/header.tsx
+++ modules/dashboard/views/ui/settings/header.tsx
@@ -1,38 +1,30 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { LogOut, Trash2 } from "lucide-react";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useAuthState } from "@/components/providers/auth-context";
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { authClient } from "@/lib/auth-client";
-import LeaveTeam from "@/modules/auth/views/ui/leave-team";
 import { useApp } from "@/modules/providers/middleware";
-import { useTRPC } from "@/trpc/client";
+import { Skeleton } from "@/components/ui/skeleton";
+import DeleteTeam from "../danger/delete-team";
+import LeaveTeam from "@/modules/auth/views/ui/leave-team";
+import { Button } from "@/components/ui/button";
+import { LogOut, Trash2 } from "lucide-react";
 
 export default function TeamSettingsHeader({
   loading,
   data,
 }: {
-  data: string;
+  data?: string;
   loading: boolean;
 }) {
   const { organization } = useApp();
 
   return (
     <div>
-      {/*<div className="mt-6">
-        <h1 className="text-sm font-medium">Settings</h1>
-      </div>*/}
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-lg font-medium">{organization?.name}</h1>
           <p className="text-sm text-foreground/80">
             {loading ? (
               <Skeleton className="h-3 w-40" />
-            ) : data == "owner" ? (
+            ) : data === "owner" ? (
               "You created this team"
             ) : (
               "You are a member of this team"
@@ -42,13 +34,13 @@ export default function TeamSettingsHeader({
         {loading ? (
           <Skeleton className="h-10 w-28" />
         ) : data == "owner" ? (
-          <Button asChild>
-            <Link href={`/~/${organization?.id}/danger`}>
+          <DeleteTeam teamId={organization?.id!}>
+            <Button>
               Delete <Trash2 className="size-3.5" />
-            </Link>
-          </Button>
+            </Button>
+          </DeleteTeam>
         ) : (
-          <LeaveTeam slug={organization?.id as string}>
+          <LeaveTeam slug={organization?.id!}>
             <Button>
               Leave <LogOut className="size-3.5" />
             </Button>
