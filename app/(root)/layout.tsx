5b49aefea229b5d5956c8ed65174aab5cfaac0db
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-08-10 01:20:03 +0530

    feat: remove global header from root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,7 @@
-import Header from "@/modules/landing/views/ui/header";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <main>
-      <Header />
-      {children}
-    </main>
-  );
+  return <main>{children}</main>;
 }
