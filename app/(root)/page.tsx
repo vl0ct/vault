6f731a3789317a112640f4110a64dc009e5372f9
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-08-10 01:20:18 +0530

    feat: compose landing sections into landing grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
 import Features from "@/modules/landing/views/ui/features";
+import Footer from "@/modules/landing/views/ui/footer";
 import Hero from "@/modules/landing/views/ui/hero";
+import LandingGrid from "@/modules/landing/views/ui/landing-grid";
 import Preview from "@/modules/landing/views/ui/preview";
 
 export default function Home() {
   return (
-    <div>
+    <LandingGrid>
       <Hero />
-      <Features />
       <Preview />
-    </div>
+      <Features />
+      <Footer />
+    </LandingGrid>
   );
 }
