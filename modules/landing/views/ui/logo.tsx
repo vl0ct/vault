ccd568dafef6e117bd2c650491bddb37f1cb8ace
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-08-10 01:23:51 +0530

    feat: update logo

--- modules/landing/views/ui/logo.tsx
+++ modules/landing/views/ui/logo.tsx
@@ -1,12 +1,13 @@
 import { Container } from "lucide-react";
 import Link from "next/link";
 
-export const Logo = () => (
-  <Link
-    href="/"
-    className="flex items-center gap-1 w-fit px-2 py-0.5 hover:bg-secondary"
-  >
-    <Container className="h-4! w-4!" />
-    <h1 className="text-lg font-bold">Vault</h1>
-  </Link>
-);
+export const Logo = ({ className }: { className?: string }) => {
+  return (
+    <span className={`${className}`}>
+      <Link href="/" className="flex items-center gap-1">
+        <Container className="h-7 w-7 mr-1" />
+        <span className="text-3xl font-bold tracking-tight">Vault</span>
+      </Link>
+    </span>
+  );
+};
